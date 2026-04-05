7ed766481882cb0b19aa79dcf237c0867f6a42ea
Federico-Roda97 <federicofede1997@gmail.com>
2026-04-05 16:19:03 +0200

    initial project structure

---
+++ src/JavaScript.js
@@ -0,0 +1,15 @@
+/* import { createInterface } from "readline/promises";
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function main() {
+    const nome = await rl.question("Come ti chiami? ");
+    const eta = await rl.question("Quanti anni hai? ");
+    console.log(`Ciao ${nome}, hai ${eta} anni!`);
+    rl.close();
+}
+
+main(); */
